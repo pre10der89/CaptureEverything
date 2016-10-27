468d74f752de79269077b127d0d5039e303fa04c
Andrew Forster <anforste@bluejeansnet.com>
2016-10-27 10:21:00 -0700

    Removing unnecessary using statements... Clearing SA warnings...

--- CaptureEverything.Common/Class1.cs
+++ CaptureEverything.Common/Class1.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-//Added in master
+﻿// <copyright file="Class1.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace CaptureEverything.Common
 {
-    class Class1
+#pragma warning disable SA1600 // Elements must be documented
+    internal class Class1
+#pragma warning restore SA1600 // Elements must be documented
     {
     }
 }
